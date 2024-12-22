d35428a15e11cfd451bc6de408743f6bdbdc0e38
Mauricio Palma <mauricio-antonio-pg@outlook.com>
2024-12-22 17:23:28 -0600

    Fix column issue

--- app.py
+++ app.py
@@ -6,14 +6,16 @@ car_data = pd.read_csv('vehicles_us.csv')
 
 st.header('Panel de Control de Ventas de Vehículos')
 
+# Botón para histograma usando 'cases'
 hist_button = st.button('Construir Histograma')
 if hist_button:
-    st.write('Histograma del odómetro')
-    fig = px.histogram(car_data, x='odometer')
+    st.write('Histograma de casos')
+    fig = px.histogram(car_data, x='cases')
     st.plotly_chart(fig, use_container_width=True)
 
+# Botón para gráfico de dispersión usando 'cases' y 'deaths'
 scatter_button = st.button('Construir Gráfico de Dispersión')
 if scatter_button:
-    st.write('Gráfico de Dispersión entre odómetro y precio')
-    fig = px.scatter(car_data, x='odometer', y='price')
+    st.write('Gráfico de Dispersión entre casos y muertes')
+    fig = px.scatter(car_data, x='cases', y='deaths')
     st.plotly_chart(fig, use_container_width=True)
