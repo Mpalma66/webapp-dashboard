97c0d59dcd80e768c1a97cdcfefa261b2945b6fe
Mauricio Palma <mauricio-antonio-pg@outlook.com>
2024-12-22 18:19:34 -0600

    Descargar CSV desde Google Drive

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import requests
 
-# Leer el dataset actualizado
-car_data = pd.read_csv('vehicles_us.csv')
+# URL del archivo CSV (Google Drive o Dropbox)
+csv_url = 'https://drive.google.com/file/d/1ZRPsX_IogWxUSJnK6sYpdFkRCcp4kP8a/view?usp=sharingID'
+
+@st.cache
+def load_data(url):
+    response = requests.get(url)
+    with open('vehicles_us.csv', 'wb') as f:
+        f.write(response.content)
+    return pd.read_csv('vehicles_us.csv')
+
+car_data = load_data(csv_url)
 
 st.header('Panel de Control de Ventas de Vehículos')
 
-# Botón para histograma del odómetro
 hist_button = st.button('Construir Histograma')
 if hist_button:
     st.write('Histograma del odómetro')
     fig = px.histogram(car_data, x='odometer')
     st.plotly_chart(fig, use_container_width=True)
 
-# Botón para gráfico de dispersión (odómetro vs precio)
 scatter_button = st.button('Construir Gráfico de Dispersión')
 if scatter_button:
     st.write('Gráfico de Dispersión entre odómetro y precio')
